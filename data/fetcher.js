c0debf6f32cb1f5b920d3f57d50ea44d99a67f44
matt novotny <mattrnovotny@gmail.com>
2024-07-01 12:50:28 -0500

    fixed up

--- data/fetcher.js
+++ data/fetcher.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8000'
 
 const checkError = (res) => {
   if (!res.ok) {
@@ -9,25 +9,28 @@ const checkError = (res) => {
 
 const checkErrorJson = (res) => {
   if (!res.ok) {
-    throw Error(res.status);
-  } else {
-    return res.json()
+    return res.json().then(error => {
+      // Create and throw a custom error object
+      throw { status: res.status, message: error.message || res.statusText };
+    });
   }
-}
+  return res.json();
+};
+
 
 const catchError = (err) => {
   if (err.status === 401) {
     window.location.href = "/login"
   }
-  if (err.status === 404) {
-    throw Error(err.message);
+  if (err.status === 404 || err.status === 400) {
+    throw err
   }
 }
 
-export const fetchWithResponse = (resource, options) => fetch(${API_URL}/${resource}, options)
+export const fetchWithResponse = (resource, options) => fetch(`${API_URL}/${resource}`, options)
   .then(checkErrorJson)
   .catch(catchError)
 
-export const fetchWithoutResponse = (resource, options) => fetch(${API_URL}/${resource}, options)
+export const fetchWithoutResponse = (resource, options) => fetch(`${API_URL}/${resource}`, options)
   .then(checkError)
   .catch(catchError)
